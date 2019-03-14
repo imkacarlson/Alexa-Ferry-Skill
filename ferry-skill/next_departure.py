adbcdde8def3ceeb458af5fab9f8d893d7fe1d80
Keith Carlson <imkacarlson@gmail.com>
2019-03-14 11:19:02 -0700

    Added weekedn functionality

--- ferry-skill/next_departure.py
+++ ferry-skill/next_departure.py
@@ -7,17 +7,28 @@ def next_departure_times(num_future_departures_wanted = 3):
     # holidays = []
     
     d = datetime.datetime.today()
-    weekday_departures = [x for x in df["weekday"].dropna() if (int(x.split(":")[0]) >= d.hour and int(x.split(":")[1]) > d.minute)][0:num_future_departures_wanted]
-    if(len(weekday_departures) < 3):
+
+    # Today is a weekday
+    if((d.weekday() >= 1) and (d.weekday() <= 5)):
+        departures = [x for x in df["weekday"].dropna() if (int(x.split(":")[0]) >= d.hour and int(x.split(":")[1]) > d.minute)][0:num_future_departures_wanted]
+    # Today is a Saturday
+    elif(d.weekday() == 6):
+        departures = [x for x in df["holiday_saturday"].dropna() if (int(x.split(":")[0]) >= d.hour and int(x.split(":")[1]) > d.minute)][0:num_future_departures_wanted]
+    # Today is a Sunday
+    elif(d.weekday() == 7):
+        departures = [x for x in df["sunday"].dropna() if (int(x.split(":")[0]) >= d.hour and int(x.split(":")[1]) > d.minute)][0:num_future_departures_wanted]
+
+
+    if(len(departures) < 3):
         # Tomorrow is a Saturday
         if((d.weekday()) + 1 == 5):
-            return(weekday_departures + list(df["holiday_saturday"].dropna()[0:(num_future_departures_wanted - len(weekday_departures))]))
+            return(departures + list(df["holiday_saturday"].dropna()[0:(num_future_departures_wanted - len(departures))]))
         #Tomorrow is a Sunday
         elif(d.weekday() + 1 == 6):
-            return (weekday_departures + list(df["sunday"].dropna()[0:(num_future_departures_wanted - len(weekday_departures))]))
+            return (departures + list(df["sunday"].dropna()[0:(num_future_departures_wanted - len(departures))]))
         # Tomorrow is a Monday
         elif(d.weekday() + 1 == 7):
-            return (weekday_departures + list(df["weekday"].dropna()[0:(num_future_departures_wanted - len(weekday_departures))]))
+            return (departures + list(df["weekday"].dropna()[0:(num_future_departures_wanted - len(departures))]))
 
-    return weekday_departures[0:num_future_departures_wanted]
+    return departures[0:num_future_departures_wanted]
             
