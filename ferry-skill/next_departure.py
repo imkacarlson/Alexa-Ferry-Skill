58e7a8dc3010333e412fb75910555c1230d5fcb6
Keith Carlson <imkacarlson@gmail.com>
2019-03-15 14:02:12 -0700

    Replaced the use of csv file with JSON file

--- ferry-skill/next_departure.py
+++ ferry-skill/next_departure.py
@@ -1,34 +1,37 @@
-import pandas as pd
+import json
 import datetime
 
-def next_departure_times(num_future_departures_wanted = 3):
+def next_departure_times(num_future_departures_wanted = 3, departure_terminal = "bainbridge", arrival_terminal = 'seattle'):
+    with open('bainbridge_departures_2013.json') as f:
+        departure_times = json.loads(json.load(f))[departure_terminal]
     
-    df = pd.read_csv("Bainbridge_Departure_Times_2013.csv", dtype=object)
     # holidays = []
     
     d = datetime.datetime.today()
 
     # Today is a weekday
     if((d.weekday() >= 1) and (d.weekday() <= 5)):
-        departures = [x for x in df["weekday"].dropna() if (int(x.split(":")[0]) >= d.hour and int(x.split(":")[1]) > d.minute)][0:num_future_departures_wanted]
+        departures = [x for x in departure_times["weekday"] if (int(x.split(":")[0]) >= d.hour and int(x.split(":")[1]) > d.minute)]
     # Today is a Saturday
     elif(d.weekday() == 6):
-        departures = [x for x in df["holiday_saturday"].dropna() if (int(x.split(":")[0]) >= d.hour and int(x.split(":")[1]) > d.minute)][0:num_future_departures_wanted]
+        departures = [x for x in departure_times["holiday_saturday"] if (int(x.split(":")[0]) >= d.hour and int(x.split(":")[1]) > d.minute)]
     # Today is a Sunday
     elif(d.weekday() == 7):
-        departures = [x for x in df["sunday"].dropna() if (int(x.split(":")[0]) >= d.hour and int(x.split(":")[1]) > d.minute)][0:num_future_departures_wanted]
+        departures = [x for x in departure_times["sunday"] if (int(x.split(":")[0]) >= d.hour and int(x.split(":")[1]) > d.minute)]
 
 
     if(len(departures) < 3):
         # Tomorrow is a Saturday
         if((d.weekday()) + 1 == 5):
-            return(departures + list(df["holiday_saturday"].dropna()[0:(num_future_departures_wanted - len(departures))]))
+            return(departures + list(departure_times["holiday_saturday"][0:(num_future_departures_wanted - len(departures))]))
         #Tomorrow is a Sunday
         elif(d.weekday() + 1 == 6):
-            return (departures + list(df["sunday"].dropna()[0:(num_future_departures_wanted - len(departures))]))
+            return (departures + list(departure_times["sunday"][0:(num_future_departures_wanted - len(departures))]))
         # Tomorrow is a Monday
         elif(d.weekday() + 1 == 7):
-            return (departures + list(df["weekday"].dropna()[0:(num_future_departures_wanted - len(departures))]))
+            return (departures + list(departure_times["weekday"][0:(num_future_departures_wanted - len(departures))]))
 
     return departures[0:num_future_departures_wanted]
-            
+
+
+print(next_departure_times())      
