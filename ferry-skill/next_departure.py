f583afa333229f888c361bda3232e3228be85b80
Keith Carlson <imkacarlson@gmail.com>
2019-03-18 12:10:51 -0700

    Reworked logic that gets next departures

--- ferry-skill/next_departure.py
+++ ferry-skill/next_departure.py
@@ -8,11 +8,12 @@ def next_departure_times(num_future_departures_wanted = 3, departure_terminal = 
     # holidays = []
     
     d = datetime.datetime.today()
+    print(d)
 
     # Today is a weekday
     if((d.weekday() >= 0) and (d.weekday() <= 4)):
-        #departures = [x for x in departure_times["weekday"] if (int(x.split(":")[0]) >= d.hour and int(x.split(":")[1]) > d.minute)]
         departures = [x for x in departure_times["weekday"] if int(x.split(":")[0]) >= d.hour]
+        departures = [x for x in departures if (departure_time_to_timestamp(x, d) > d)]
     # Today is a Saturday
     elif(d.weekday() == 5):
         departures = [x for x in departure_times["holiday_saturday"] if int(x.split(":")[0]) >= d.hour]
@@ -33,3 +34,7 @@ def next_departure_times(num_future_departures_wanted = 3, departure_terminal = 
             return (departures + list(departure_times["weekday"][0:(num_future_departures_wanted - len(departures))]))
 
     return departures[0:num_future_departures_wanted]
+
+def departure_time_to_timestamp(departure_time, today):
+    hour_min = departure_time.split(":")
+    return today.replace(hour = int(hour_min[0]), minute = int(hour_min[1]))
