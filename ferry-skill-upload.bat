b6abd85340c88af5d7dbcb29abbac7244693454d
Keith Carlson <imkacarlson@gmail.com>
2019-04-17 14:05:21 -0700

    Reoganizing

--- ferry-skill-upload.bat
+++ ferry-skill-upload.bat
@@ -1,7 +1,7 @@
 @echo off
 
 del index.zip
-cd .\ferry-skill\
+cd .\ferry-skill-lambda\
 7z a -r ..\index.zip *
 cd ..
 aws lambda update-function-code --function-name ferry-skill --zip-file fileb://index.zip
